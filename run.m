1f0679c887b201864046128bab23c0fcafef8af6
evoup <yinjia@madhouse-inc.com>
2017-06-26 22:28:57 +0800

    c

--- run.m
+++ run.m
@@ -6,4 +6,17 @@ y=y'
 %plot(X,y)
 C = 1
 sigma = 0.1
+%M2 = dlmread("feature1.txt")
+[input] = textread('feature1.txt','%s');
+
+
+fprintf('---------解压特征-------------\n')
+[input] = textread( 'feature1.txt', '%s' , 'delimiter' , '\n' , 'headerlines' , 1)
+[num_features,]=size(input)
+for x=1:num_features
+    fprintf('%d\n' , x)
+end
+
+
+
 model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
