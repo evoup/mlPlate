0232206ac241bfd308174ff90fc1ec8797b16c8b
evoup <yinjia@madhouse-inc.com>
2017-06-26 21:40:35 +0800

    add function

--- run.m
+++ run.m
@@ -1,3 +1,9 @@
- C = 0.1;
 % X是特征向量1，y是类别编号
- model = svmTrain(X, y, C, @linearKernel);  
+X=[1,2,3]
+y=[4,5,6]
+X=X'
+y=y'
+%plot(X,y)
+C = 1
+sigma = 0.1
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
