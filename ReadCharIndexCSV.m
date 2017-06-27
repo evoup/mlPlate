bb03fb2ada9c5e5c14c8f1a6ed056189247164b6
evoup <evoex123@gmail.com>
2017-06-27 23:27:35 +0800

    c

--- ReadCharIndexCSV.m
+++ ReadCharIndexCSV.m
@@ -2,7 +2,7 @@
 fid = fopen('Char_Index.txt');
 
 tline = fgets(fid);
-v=[]
+vc=[]
 while ischar(tline)
     disp(tline)
     tline = fgets(fid); % 正好跳过第一个不需要的“序号 类别编号 对应图片”
@@ -10,8 +10,8 @@ while ischar(tline)
         c=strsplit(tline)
         c{1} % 4为自然序号，显然用不着
         c{2} % 实际的文字索引直接写入向量
-        v=[v,str2num(c{2})]
+        vc=[v,str2num(c{2})]
     end
 end
 fclose(fid);
-v
+vc
